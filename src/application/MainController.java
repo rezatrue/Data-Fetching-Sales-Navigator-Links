08c1a56a42e4ddf67370d43e2e1efb17edb391b2
rezatrue <rezatrue@gmail.com>
2017-12-28 14:36:44 +0600

    csv file load complete

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.io.File;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class MainController implements Initializable {
 	@FXML
@@ -14,15 +18,37 @@ public class MainController implements Initializable {
 	@FXML
 	private TextField textNumber;
 
+	private LinkedList<Info> list = null;
+
 	@FXML
 	public void selectFileBtnAction(ActionEvent event) {
-		System.out.println("Selcted Button");
-		textFileName.setText("FILE NAME");
+		// System.out.println("Selcted Button");
+		// textFileName.setText("FILE NAME");
+
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		File file = fileChooser.showOpenDialog(new Stage());
+		String filePath = file.getAbsolutePath();
+
+		CSV_Scanner csv_Scanner = new CSV_Scanner();
+		if (filePath.endsWith(".csv")) {
+			list = csv_Scanner.dataScan(filePath);
+			textFileName.setText(filePath);
+		} else
+			System.out.println("Not a CSV FILE");
+
+		// possible errors: file is not properly formated
+		// wrong file
+		// null file / no data
+
 	}
 
 	@FXML
 	public void startBtnAction(ActionEvent event) {
-		System.out.println("Start Button : " + textNumber.getText().toString());
+		// System.out.println("Start Button : " +
+		// textNumber.getText().toString());
+
+		System.out.println(list.get(0).getLink());
 	}
 
 	@FXML
