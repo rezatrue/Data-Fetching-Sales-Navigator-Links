329f00ead5a9a12b040466b69eb2fc11aa27438f
rezatrue <rezatrue@gmail.com>
2018-07-21 21:30:53 +0600

    icon added

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,10 +5,13 @@ package application;
  * 
  */
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,6 +23,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Linkedin list Fetcher : 2.00");
+			primaryStage.getIcons().add(new Image(new File("image/linkedin_icon.gif").toURI().toString()));
 			primaryStage.setResizable(false);
 			primaryStage.show();
 
