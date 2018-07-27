7e74ec81bb569bc464f8aa8e46e2c4333e520e94
rezatrue <rezatrue@gmail.com>
2018-07-27 20:19:10 +0600

    SQLite tesing code added

---
+++ src/db/DBTester.java
@@ -0,0 +1,18 @@
+package db;
+
+public class DBTester {
+
+	public static void main(String[] args) {
+		LocalDBHandler dbHandler = new LocalDBHandler();
+		
+		dbHandler.connect();
+		dbHandler.createNewTable();
+		dbHandler.insert("https://www.linkedin.com/sales/profile/AAEAABesfSMBkXdMJ8Ddo2No8aZf4TsDVf9CZC8,067m,NAME_SEARCH?moduleKey=peopleSearchResults", "Farid", "Miah", "Upwork");
+		dbHandler.insert("https://www.linkedin.com/sales/profile/AAEAACSd2CQBq5uBq8l4xk0hr7S_ibyF1Elb2AA,_GhY,NAME_SEARCH?moduleKey=peopleSearchResults", "Md", "Monir Khondoker", "Upwork");
+		dbHandler.insert("https://www.linkedin.com/sales/profile/AAEAACU8wMABUeAxd3qJhRbq1IU5_nD9ivq92vQ,_V-D,NAME_SEARCH?moduleKey=peopleSearchResults", "Md", "RASEL", "Upwork");
+		dbHandler.insert("https://www.linkedin.com/sales/profile/AAEAAABmEMABPIfO4QbP0NB4HE0TpuLFhWqc4TE,s6ma,NAME_SEARCH?moduleKey=peopleSearchResults", "Gina", "Melani Caulkins", "Upwork");
+		dbHandler.selectAll();
+		dbHandler.closeConnection();
+	}
+
+}
--- src/db/LocalDBHandler.java
+++ src/db/LocalDBHandler.java
@@ -6,38 +6,147 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LocalDBHandler {
 	
-	
+	private final String DB_NAME = "llf.db";
+	private String TABLE_NAME = "profile";
+	private Connection conn = null;
 
-	public static void main(String[] args) {
-		connect();
+	public LocalDBHandler() {
 	}
-	
-	public static void connect() {
-	        Connection conn = null;
+		
+	public void connect() {
 	        try {
-	            // db parameters
-	            String url = "jdbc:sqlite:sqlite/db/llf.db";
-	            // create a connection to the database
+	            String url = "jdbc:sqlite:sqlite/db/" + DB_NAME;
 	            conn = DriverManager.getConnection(url);
 	            
 	            System.out.println("Connection to SQLite has been established.");
 	            
 	        } catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	        } finally {
-	            try {
-	                if (conn != null) {
-	                    conn.close();
-	                }
-	            } catch (SQLException ex) {
-	                System.out.println(ex.getMessage());
-	            }
+	            System.out.println("0"+e.getMessage());
 	        }
 	    }
 	
+	public void closeConnection() {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("10"+ex.getMessage());
+            }
+	}
+	
+	public void dropTable() {
+		
+	}
+	
+	public void createNewTable() {
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS "+ TABLE_NAME + " ("
+                + "	Linkedin_Profile_URL text PRIMARY KEY,"
+                + "	First_Name text,"
+                + "	Last_Name text,"
+                + "	Email_ID text,"
+                + "	Contact_Number text,"
+                + "	Location text,"
+                + "	Industry text,"
+                + "	Designation text,"
+                + "	Company_Name text,"
+                + "	Company_Size text"
+                + ");";
+        
+        if(conn==null)
+        	connect();
+        
+        Statement stmt = null;
+        
+        try {
+			if(conn!=null) {
+				stmt = conn.createStatement();
+			    stmt.execute(sql);
+			}
+		} catch (SQLException e) {
+			System.out.println("1"+e.getMessage());
+		}finally {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				System.out.println("2"+e.getMessage());
+			}
+		}
+    }
+	
+	public void selectAll(){
+        String sql = "SELECT * FROM " + TABLE_NAME;
+        if(conn == null)
+        	connect();
+        
+        Statement stmt = null;
+        ResultSet rs = null;
+        if(conn!=null) {
+        	
+        	try {
+				stmt  = conn.createStatement();
+				rs    = stmt.executeQuery(sql);
+				// loop through the result set
+				while (rs.next()) {
+				    System.out.println( 
+				                       rs.getString("Linkedin_Profile_URL") + "\t" +
+				                       rs.getString("First_Name") +  "\t" + 
+				                       rs.getString("Last_Name") + "\t" +
+				                       rs.getString("Email_ID") +  "\t" + 
+				                       rs.getString("Contact_Number") + "\t" +
+				                       rs.getString("Location") +  "\t" + 
+				                       rs.getString("Industry") + "\t" +
+				                       rs.getString("Designation") +  "\t" + 
+				                       rs.getString("Company_Name") + "\t" +
+				                       rs.getString("Company_Size"));
+				}
+			} catch (SQLException e) {
+				System.out.println("5"+e.getMessage());
+			}finally {
+				try {
+					rs.close();
+					stmt.close();
+				} catch (SQLException e) {
+					System.out.println("6"+e.getMessage());
+				}
+				
+			}
+			
+        }
+	}
+
+	public void insert(String link, String fname, String lname, String company) {
+        String sql = "INSERT INTO "+ TABLE_NAME + " (Linkedin_Profile_URL, First_Name, Last_Name, Company_Name) VALUES(?,?,?,?)";
+        PreparedStatement pstmt = null;
+        if(conn == null)
+        	connect();
+        if(conn != null) {
+	        try {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, link);
+				pstmt.setString(2, fname);
+				pstmt.setString(3, lname);
+				pstmt.setString(4, company);
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				System.out.println("3"+e.getMessage());
+			}finally {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					System.out.println("4"+e.getMessage());
+				}
+			}
+        }  
+    }
 	
 }
